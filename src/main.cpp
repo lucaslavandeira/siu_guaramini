2be3e34a163897d527f47e4a3d28bbb842a7b26f
Lucas L <lucaslavandeira@gmail.com>
2017-04-20 17:25:38 -0300

    Add main

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    std::cout << "Hola mundo" << std::endl;
+    std::string hola;
+    std::cin >> hola;
+    std::cout << "You entered:" << hola << std::endl;
+    return 0;
+}
