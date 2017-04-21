ee066097af954cd1dcb8d4ac4aaa22e4c67fca76
Lucas L <lucaslavandeira@gmail.com>
2017-04-21 20:23:40 -0300

    Add command parsing

--- src/main.cpp
+++ src/main.cpp
@@ -2,24 +2,45 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <map>
 #include "common_split.h"
 
+const std::map<std::string, std::string> commands {
+    {std::string("listarMaterias"), std::string("lm")},
+    {std::string("listarInscripciones"), std::string("li")},
+    {std::string("inscribir"), std::string("in")},
+    {std::string("desinscribir"), std::string("de")}
+};
+
+
+void parse_command(std::string& input) {
+    std::vector<std::string> params = split(input, ' ');
+    std::string cmd;
+    try {
+        cmd = commands.at(params.at(0));
+    } catch (std::out_of_range) {
+        std::cout << "Wrong command!" << std::endl;
+        return;
+    }
+
+    std::string result;
+    result += cmd;
+
+    for (auto it = params.begin()+1; it != params.end(); it ++) {
+        result += "-" + *it;
+    }
+    std::cout << result;
+}
+
 int main(int argc, char** argv) {
-    std::fstream f;
+   /*std::fstream f;
     f.open("materias.txt");
     char line[256];
     f.getline(line, 256);
-    std::string input(line);
-    std::cout << input << std::endl;
-    std::vector<std::string> a = split(input, '\t');
-
-    for (std::string s : a) {
-        std::cout << s << std::endl;
-    }
-
+    std::string input(line);*/
+    std::string input;
     getline(std::cin, input);
-    std::cout << "input was: " << input << std::endl;
+
+    parse_command(input);
     return 0;
 }
-
-
