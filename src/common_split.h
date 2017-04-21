861fdf5b16bedeecbdf817d1eca13cfd852e190c
Lucas L <lucaslavandeira@gmail.com>
2017-04-21 18:48:31 -0300

    Add split common function

---
+++ src/common_split.h
@@ -0,0 +1,8 @@
+#ifndef TP3TALLER_COMMON_SPLIT_H
+#define TP3TALLER_COMMON_SPLIT_H
+
+#endif //TP3TALLER_COMMON_SPLIT_H
+#include <vector>
+#include <string>
+
+std::vector<std::string> split(std::string& input, char delim);
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,25 @@
 #include <iostream>
+#include <fstream>
 #include <string>
+#include <vector>
+#include "common_split.h"
 
-int main() {
-    std::cout << "Hola mundo" << std::endl;
-    std::string hola;
-    std::cin >> hola;
-    std::cout << "You entered:" << hola << std::endl;
+int main(int argc, char** argv) {
+    std::fstream f;
+    f.open("materias.txt");
+    char line[256];
+    f.getline(line, 256);
+    std::string input(line);
+    std::cout << input << std::endl;
+    std::vector<std::string> a = split(input, '\t');
+
+    for (std::string s : a) {
+        std::cout << s << std::endl;
+    }
+
+    getline(std::cin, input);
+    std::cout << "input was: " << input << std::endl;
     return 0;
 }
+
+
